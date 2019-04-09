00d72ef3478f86928c0c21b342b56f7bcc364fa1
Vinicius <viniciusvicentini@live.com>
2019-04-08 22:39:53 -0300

    login page OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthGuard } from './core/auth/auth-guard.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'batfood';
+
+  constructor(public authGuard: AuthGuard) { }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  list = [{
+    name: 'Pizza Pan',
+    category: 'Pizzaria',
+    price: 5,
+    time: 30
+  },{
+    name: 'Pizza Hut',
+    category: 'Pizzaria',
+    price: 3,
+    time: 40
+  },{
+    name: 'Pizza Domino\'s',
+    category: 'Pizzaria',
+    price: 2,
+    time: 50
+  },{
+    name: 'Pizza Fan',
+    category: 'Pizzaria',
+    price: 10,
+    time: 10
+  }]
+
   constructor() { }
 
   ngOnInit() {
