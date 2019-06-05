ca2e8285f1f37739aa21ba5227b88c190ef7685e
Vinicius <viniciusvicentini@live.com>
2019-06-05 18:53:02 -0300

    remove fixed values

--- src/app/pages/shopping/shopping.component.ts
+++ src/app/pages/shopping/shopping.component.ts
@@ -13,58 +13,58 @@ export class ShoppingComponent implements OnInit {
    * Lista de produtos
    */
   products = [
-    {
-      id: 1,
-      categoria_id: 2,
-      imagem:
-        'https://www.receiteria.com.br/wp-content/uploads/receitas-de-coracao-de-galinha-3.jpg',
-      preco: 20,
-      descricao: 'Coração de Frango',
-      nome: 'Coração kg',
-    },
-    {
-      id: 2,
-      categoria_id: 1,
-      imagem:
-        'https://res-2.cloudinary.com/gaveteiro/image/upload/c_pad,h_283,w_283/v1439315065/urfcx04zx7myn4mjpalk.jpg',
-      preco: 20,
-      descricao: 'Coca-Coca',
-      nome: 'Coca-Coca Lata',
-    },
-    {
-      id: 1,
-      categoria_id: 2,
-      imagem:
-        'https://www.receiteria.com.br/wp-content/uploads/receitas-de-coracao-de-galinha-3.jpg',
-      preco: 20,
-      descricao: 'Coração de Frango',
-      nome: 'Coração kg',
-    },
-    {
-      id: 2,
-      categoria_id: 1,
-      imagem:
-        'https://res-2.cloudinary.com/gaveteiro/image/upload/c_pad,h_283,w_283/v1439315065/urfcx04zx7myn4mjpalk.jpg',
-      preco: 20,
-      descricao: 'Coca-Coca',
-      nome: 'Coca-Coca Lata',
-    },
+    // {
+    //   id: 1,
+    //   categoria_id: 2,
+    //   imagem:
+    //     'https://www.receiteria.com.br/wp-content/uploads/receitas-de-coracao-de-galinha-3.jpg',
+    //   preco: 20,
+    //   descricao: 'Coração de Frango',
+    //   nome: 'Coração kg',
+    // },
+    // {
+    //   id: 2,
+    //   categoria_id: 1,
+    //   imagem:
+    //     'https://res-2.cloudinary.com/gaveteiro/image/upload/c_pad,h_283,w_283/v1439315065/urfcx04zx7myn4mjpalk.jpg',
+    //   preco: 20,
+    //   descricao: 'Coca-Coca',
+    //   nome: 'Coca-Coca Lata',
+    // },
+    // {
+    //   id: 1,
+    //   categoria_id: 2,
+    //   imagem:
+    //     'https://www.receiteria.com.br/wp-content/uploads/receitas-de-coracao-de-galinha-3.jpg',
+    //   preco: 20,
+    //   descricao: 'Coração de Frango',
+    //   nome: 'Coração kg',
+    // },
+    // {
+    //   id: 2,
+    //   categoria_id: 1,
+    //   imagem:
+    //     'https://res-2.cloudinary.com/gaveteiro/image/upload/c_pad,h_283,w_283/v1439315065/urfcx04zx7myn4mjpalk.jpg',
+    //   preco: 20,
+    //   descricao: 'Coca-Coca',
+    //   nome: 'Coca-Coca Lata',
+    // },
   ]
 
   /**
    * Lista de categorias
    */
   categories = [
-    {
-      id: 1,
-      descricao: 'Bebida',
-      nome: 'Bebida',
-    },
-    {
-      id: 2,
-      descricao: 'Comida',
-      nome: 'Comida',
-    },
+    // {
+    //   id: 1,
+    //   descricao: 'Bebida',
+    //   nome: 'Bebida',
+    // },
+    // {
+    //   id: 2,
+    //   descricao: 'Comida',
+    //   nome: 'Comida',
+    // }
   ]
 
   /**
